6adef54f8bb4190ece69a138587afbedb35cb6b4
ibotdotout <tkroputa@gmail.com>
2015-11-20 18:16:04 +0700

    enable boostrap sass

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'bcrypt', '~> 3.1.7'
 # gem 'capistrano-rails', group: :development
 
 gem 'devise'
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119145343) do
+ActiveRecord::Schema.define(version: 20151119172933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "csvfiles", force: :cascade do |t|
+    t.binary   "file_contents"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "csvfiles", ["user_id"], name: "index_csvfiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 20151119145343) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "csvfiles", "users"
 end
