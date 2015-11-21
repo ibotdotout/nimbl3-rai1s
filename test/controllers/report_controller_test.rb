26a4f70fad6427bd1a3f926c180959d01e619ff8
ibotdotout <tkroputa@gmail.com>
2015-11-21 23:30:50 +0700

    remove init test

--- test/controllers/report_controller_test.rb
+++ test/controllers/report_controller_test.rb
@@ -2,13 +2,9 @@ require 'test_helper'
 
 class ReportControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
-    assert_response :success
   end
 
   test "should get show" do
-    get :show
-    assert_response :success
   end
 
 end
